d413a4c821c9667c4db5b091e08d050d2564bdb7
Veggieplanner <holzunger@aol.de>
2025-04-27 13:05:29 +0200

    Delete app/config.py

--- app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Standard-Konfiguration
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "your-key-here")
-
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///data.db")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
